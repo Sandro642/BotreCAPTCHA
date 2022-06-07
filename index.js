d8272c3bc6f09a76eaeebac9ce7a1ec50b0167bb
sandro <84270313+Sandro642@users.noreply.github.com>
2022-06-07 23:32:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ console.log("Bot : reCAPTCHA | ON");
 client.user.setActivity("Verified Connexion Web");
 });
 
-client.login('OTc5ODcwNDM0MjgxNTM3NTc2.GkHTKl.LhsZCCB5SnyOt2A_582CiXdCAFy-7UBm0aeA90');
+client.login(process.env.TOKEN);
